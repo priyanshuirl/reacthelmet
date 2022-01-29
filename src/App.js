6c755fde0d18b2dea4a06d56636a1d4ba6f37fa7
Priyanshu Mishra <priyanshu12651265@gmail.com>
2022-01-29 17:30:41 +0530

    Added sample meta tags in index html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <h1>Testing React Router</h1>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">page1</Link>
+            </li>
+            <li>
+              <Link to="/users">page2</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>Page 1</h2>;
+}
+
+function Users() {
+  return <h2>Page 2</h2>;
 }
 
 export default App;
